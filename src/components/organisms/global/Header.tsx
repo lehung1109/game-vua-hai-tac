d8d0ec64d58158688817cd03c8b78dd81bae231e
Hung Le <hung.le@optimizely.com>
2025-08-23 14:45:13 +0700

    update home page

--- src/components/organisms/global/Header.tsx
+++ src/components/organisms/global/Header.tsx
@@ -15,15 +15,17 @@ const Header = () => {
 
         {/* right side include login and sign up button */}
         <div>
-          <Link href="/sign-in">
-            <button className="px-4 py-2 mr-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">
-              Sign In
-            </button>
+          <Link
+            href="/sign-in"
+            className="px-4 py-2 mr-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+          >
+            Sign In
           </Link>
-          <Link href="/sign-up">
-            <button className="px-4 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300 transition">
-              Sign Up
-            </button>
+          <Link
+            href="/sign-up"
+            className="px-4 py-2 bg-gray-200 text-gray-800 rounded hover:bg-gray-300 transition"
+          >
+            Sign Up
           </Link>
         </div>
       </div>
